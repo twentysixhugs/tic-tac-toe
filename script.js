ce79064b7dbec923c5f064fac2bfd29e1dd913b9
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-01-19 22:32:41 +0300

    add gameboard object and raw display population

---
+++ script.js
@@ -0,0 +1,53 @@
+const GameBoard = (function() {
+    const _board = ['x', 'x', 'x', 'o', '', 'o', '', 'x', 'x']; //The array that stores current game state
+
+    const renderBoardToWebpage = function() {
+        const cellDivs = document.querySelectorAll('.cell');
+
+        cellDivs.forEach((cell, i) => {
+            if (_board[i]) {
+                cell.appendChild(MarksImages[_board[i]].cloneNode());
+                //Marks object has x and o properties corresponding img nodes. _board[i] is either x, o or ""
+                //If it's x or o, then it takes img node from Marks object and appends it to the current cell
+            }
+        });
+    }
+
+    const getBoard = function() {
+        return [..._board];
+    }
+
+    const setCellInBoardArray = function(index, mark) {
+        _board[index] = mark;
+    }
+
+    return {getBoard, setCellInBoardArray, renderBoardToWebpage};
+})();
+
+// const displayController = (function(_board) {
+//     const renderBoardToWebpage = function() {
+//         const cellDivs = document.querySelectorAll('.cell');
+
+//         cellDivs.forEach((cell, i) => {
+//             if (_board[i]) {
+//                 cell.appendChild(MarksImages[_board[i]].cloneNode());
+//                 //Marks object has x and o properties corresponding img nodes. _board[i] is either x, o or ""
+//                 //If it's x or o, then it takes img node from Marks object and appends it to the current cell
+//             }
+//         });
+//     }
+// })(GameBoard.getBoard());
+
+const MarksImages = (function createMarksImages() {
+    const _xImg = document.createElement('img');
+    const _oImg = document.createElement('img');
+
+    _xImg.setAttribute('src', './img/x.png');
+    _xImg.setAttribute('alt', "");
+    _oImg.setAttribute('src', './img/o.png');
+    _oImg.setAttribute('alt', "");
+
+    return {x: _xImg, o: _oImg};
+})();
+
+GameBoard.renderBoardToWebpage();
